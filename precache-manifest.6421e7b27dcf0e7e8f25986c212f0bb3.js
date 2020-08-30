2696fc2987d823c896ec61bc2ea58cabda84015c
aviram8919 <49782497+aviram8919@users.noreply.github.com>
2020-08-30 23:31:27 +0530

    Updates

---
+++ precache-manifest.6421e7b27dcf0e7e8f25986c212f0bb3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ce6af5003ce11b76350ec3411570260",
+    "url": "/My-Portfolio/index.html"
+  },
+  {
+    "revision": "6d808f3e2fd575e95e3f",
+    "url": "/My-Portfolio/static/css/main.527cfaed.chunk.css"
+  },
+  {
+    "revision": "d77a5915b721534f5001",
+    "url": "/My-Portfolio/static/js/2.1d2ca499.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/My-Portfolio/static/js/2.1d2ca499.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d808f3e2fd575e95e3f",
+    "url": "/My-Portfolio/static/js/main.6b0220b8.chunk.js"
+  },
+  {
+    "revision": "cc8fb5c09ef196e1df09",
+    "url": "/My-Portfolio/static/js/runtime-main.2dca773a.js"
+  },
+  {
+    "revision": "21ab7c5e5f0d8aa01d8b5dd69bc359e6",
+    "url": "/My-Portfolio/static/media/Profile_Picture.21ab7c5e.jpg"
+  }
+]);
